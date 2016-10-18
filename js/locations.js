25e4c871c6971cdd5a58243d4137e024d0138b63
Shafeeq Shaar <sean.spilot@gmail.com>
2016-10-18 10:02:43 +0300

    4 city markers added.

--- js/locations.js
+++ js/locations.js
@@ -2,46 +2,50 @@ $(function () {
 
     function initMap() {
 
-        var location = new google.maps.LatLng(37.768264, '-122.437259');
+        var location = new google.maps.LatLng(37.708749, '-122.293119');
 
         var mapCanvas = document.getElementById('map');
         var mapOptions = {
             center: location,
-            zoom: 13,
+            zoom: 10,
             panControl: false,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         }
         var map = new google.maps.Map(mapCanvas, mapOptions);
 
-        var markerImage = 'marker.png';
+        var markerImage = 'img/marker.png';
 
+        var location1 = new google.maps.LatLng(37.785190, '-122.424237');
         var marker = new google.maps.Marker({
-            position: location,
+            position: location1,
             map: map,
             icon: markerImage
         });
 
-        var contentString = '<div class="info-window">' +
-                '<h3>Info Window Content</h3>' +
-                '<div class="info-content">' +
-                '<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
-                '</div>' +
-                '</div>';
-
-        var infowindow = new google.maps.InfoWindow({
-            content: contentString,
-            maxWidth: 400
+        var location2 = new google.maps.LatLng(37.809775, '-122.274229');
+        var marker = new google.maps.Marker({
+            position: location2,
+            map: map,
+            icon: markerImage
         });
 
-        marker.addListener('click', function () {
-            infowindow.open(map, marker);
+        var location3 = new google.maps.LatLng(37.549465, '-121.990880');
+        var marker = new google.maps.Marker({
+            position: location3,
+            map: map,
+            icon: markerImage
+        });
+
+        var location4 = new google.maps.LatLng(37.448967, '-122.143111');
+        var marker = new google.maps.Marker({
+            position: location4,
+            map: map,
+            icon: markerImage
         });
 
         var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
         map.set('styles', styles);
     }
-
     
-
     google.maps.event.addDomListener(window, 'load', initMap);
 });
